f06d628c08c47ab73f9542cca4de173c5b374705
Joe Edwards <joe.edwards@greenlordstudios.co.uk>
2022-01-19 16:43:54 +0000

    first

---
+++ TesterAPI/Data/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesterAPI.Models;
+
+namespace TesterAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public virtual DbSet<UserData> Users { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ TesterAPI/Models/UserData.cs
@@ -0,0 +1,10 @@
+﻿namespace TesterAPI.Models
+{
+    public class UserData
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public int permlevel { get; set; }
+    }
+}
